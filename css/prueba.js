85208434ffdbc6435be1d75a702993d1a0aac71e
Rebeca Freer <rfreer@catmktg.com>
2019-09-30 20:04:51 -0600

    mi primer commit

---
+++ css/prueba.js
@@ -0,0 +1,37 @@
+// let nombre1 = "Daniela"
+// let nombre2 = "Ricardo"
+// let nombre3 = "Jimena"
+
+// console.log(nombre1)
+// console.log(nombre2)
+// console.log(nombre3)
+
+// let nombre4 = prompt("Mi nombre es")
+// console.log(nombre4)
+// let nombre5 = prompt("Mi nombre es")
+// // console.log(nombre5)
+// let nombre6 = prompt("Mi nombre es")
+// // console.log(nombre6)
+
+// let apellido7 = prompt("Mi apellido es")
+
+// let edad = prompt("Mi edad es")
+
+
+// function nombrecompleto (nombre6,apellido7, edad){
+//     return "Mi nombre es " + nombre6 + " "+  apellido7 + " y tengo" + " " + edad + " " + "años"
+// }
+
+// // console.log( nombrecompleto (nombre6, apellido7,edad))
+
+// let valor1 = prompt("Digite un valor")
+// let valor2 = prompt("digite otro valor")
+
+// let valor1num = parseInt(valor1)
+// let valor2num = parseInt(valor2)
+
+// function suma (valor1num,valor2num) {
+//     return "El total de la suma es " + (valor1num + valor2num)
+// }
+
+// console.log(suma (valor1num, valor2num))
